6fb5b9b0de4ec35fd4063ac5ec3042298f5bab13
Almog Mizrahi <almogosh111@gmail.com>
2025-11-25 14:19:41 +0200

    Enabling YouTube service

--- js/services/wikiTube.service.js
+++ js/services/wikiTube.service.js
@@ -1,9 +1,16 @@
 'use strict'
 
-const API_USERS_URL = 'https://api.github.com/users'
 
-function getCard() {
-    return get(API_USERS_URL)
-        .then(data => data)
+const API_YOUTUBE_KEY = 'AIzaSyBxciqB5_qdgXcVbpeTnr4ndENRb61OHuA'
+
+const WIKI_API = `https://en.wikipedia.org/w/api.php?origin=*&action=query&list=search&format=json&srsearch=`
+
+function getVideos(value) {
+    const YOUTUBE_URL = `https://www.googleapis.com/youtube/v3/search?part=snippet&videoEmbeddable=true&type=video&key=${API_YOUTUBE_KEY}&q=${value}`
+    return get(YOUTUBE_URL)
+        .then(data => {
+            renderVideoCards(data.items)
+            renderVideoPlayer(data.items[0].id.videoId)
+        })
         .catch(err => console.error(err))
 }
--- js/wikiTube.controller.js
+++ js/wikiTube.controller.js
@@ -5,3 +5,35 @@ function onInit() {
   console.log('WikiTube Controller Init')
 }
 
+function onSearch() {
+  const elSearchInput =document.querySelector('.search')
+  console.log('Searching for:', elSearchInput.value)
+  getVideos(elSearchInput.value)
+}
+
+function renderVideoCards(data){
+  console.log('Rendering Video Cards:', data)
+  const elSearchResult = document.querySelector('.search-result')
+  const strHtml = data.map((video) => {
+    return `<div class="video-card flex" 
+    onclick="renderVideoPlayer('${video.id.videoId}')">
+    <img src="${video.snippet.thumbnails.default.url}" alt="${video.snippet.title}">
+    <h3>${video.snippet.title}</h3> 
+    </div>`
+  })
+  elSearchResult.innerHTML = strHtml.join('')
+}
+
+function renderVideoPlayer(videoId){
+  console.log('Playing Video', videoId)
+  const elVideoPlayer = document.querySelector('.video-player')
+  if (!elVideoPlayer) {
+    console.error('video-player element not found!')
+    return
+  }
+  const strHtml = ` 
+  <h2> Video Player </h2>
+  <iframe width="420" height="345" src="https://www.youtube.com/embed/${videoId}"></iframe>
+  `
+  elVideoPlayer.innerHTML = strHtml
+}
