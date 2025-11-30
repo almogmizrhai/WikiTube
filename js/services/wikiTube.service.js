4679b439c7e284a68145e7391997150b638c91fc
Almog Mizrahi <almogosh111@gmail.com>
2025-11-30 12:21:45 +0200

    Enabling wiki service

--- js/services/wikiTube.service.js
+++ js/services/wikiTube.service.js
@@ -3,18 +3,31 @@
 
 const API_YOUTUBE_KEY = 'AIzaSyBxciqB5_qdgXcVbpeTnr4ndENRb61OHuA'
 
-const WIKI_API = `https://en.wikipedia.org/w/api.php?origin=*&action=query&list=search&format=json&srsearch=`
 
 function getVideos(value) {
     const YOUTUBE_URL = `https://www.googleapis.com/youtube/v3/search?part=snippet&videoEmbeddable=true&type=video&key=${API_YOUTUBE_KEY}&q=${value}`
     return get(YOUTUBE_URL)
-        .then(data => {
-            renderVideoCards(data.items)
-            renderVideoPlayer(data.items[0].id.videoId)
-        })
-        .catch(err => console.error(err))
-        .finally(() => {
-            const elLoader = document.querySelector('.loader')
-            elLoader.classList.add('hide')
-        })
+    .then(data => {
+        renderVideoCards(data.items)
+        renderVideoPlayer(data.items[0].id.videoId)
+    })
+    .catch(err => console.error(err))
+    .finally(() => {
+        const elLoader = document.querySelector('.loader')
+        elLoader.classList.add('hide')
+    })
+}
+
+function getWikiArticles(searchValue) {
+    const WIKI_API = `https://en.wikipedia.org/w/api.php?origin=*&action=query&list=search&format=json&srsearch=${searchValue}`
+    return get(WIKI_API)
+    .then(data => {
+        const articles = data.query.search
+        if (articles.length === 0) return 'No related Wikipedia articles found.'
+        return articles.map(article => article.snippet).join(' ')
+    })
+    .catch(err => {
+        console.error(err)
+        return 'Error fetching Wikipedia articles.'
+    })
 }
--- js/wikiTube.controller.js
+++ js/wikiTube.controller.js
@@ -8,29 +8,38 @@ function onInit() {
 function onSearch() {
   const elSearchInput =document.querySelector('.search')
   const elIntro = document.querySelector('.intro')
-  console.log('Searching for:', elSearchInput.value)
-  getVideos(elSearchInput.value)
   const elLoader = document.querySelector('.loader')
+
+  console.log('Searching for:', elSearchInput.value)
+
+  getVideos(elSearchInput.value)
+
   elLoader.classList.remove('hide')
   elIntro.classList.add('hide')
   
 }
 
 function renderVideoCards(data){
+
   console.log('Rendering Video Cards:', data)
+
   const elSearchResult = document.querySelector('.search-result')
+
   const strHtml = data.map((video) => {
     return `<div class="video-card flex" 
-    onclick="renderVideoPlayer('${video.id.videoId}')">
+    onclick="renderVideoPlayer('${video.id.videoId}' , '${video.snippet.title}')">
     <img src="${video.snippet.thumbnails.default.url}" alt="${video.snippet.title}">
     <h3>${video.snippet.title}</h3> 
     </div>`
   })
+
   elSearchResult.innerHTML = strHtml.join('')
 }
 
-function renderVideoPlayer(videoId){
-  console.log('Playing Video', videoId)
+function renderVideoPlayer(videoId,videoTitle){
+
+  console.log('Playing Video', videoId, videoTitle)
+
   const elVideoPlayer = document.querySelector('.video-player')
   if (!elVideoPlayer) {
     console.error('video-player element not found!')
@@ -41,4 +50,26 @@ function renderVideoPlayer(videoId){
   <iframe width="420" height="345" src="https://www.youtube.com/embed/${videoId}"></iframe>
   `
   elVideoPlayer.innerHTML = strHtml
+
+  renderVideoInfo(videoTitle)
+}
+
+function renderVideoInfo(videoTitle){
+
+  getWikiArticles(videoTitle)
+  .then(infoVideo => {
+    console.log('Wiki Info:', infoVideo)
+  
+    const elVideoInfo = document.querySelector('.video-info')
+    const strHtml = `
+    <h2> Video Title: ${videoTitle} </h2>
+    <p> ${infoVideo} </p>
+    `
+    elVideoInfo.innerHTML = strHtml
+  })
+
+  .catch(err => {
+    console.error(err)
+  })
+
 }
